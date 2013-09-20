6c8e19d7e7d3508c6b29d167771718a76e178319
LETARTARE <tartarinlj@laposte.net>
2013-09-20 09:20:43 -0700

    change global  directory to 'src' and create project 'GrblController-3.4.5.cbp'

---
+++ src/definitions.h
@@ -0,0 +1,51 @@
+/****************************************************************
+ * definitions.h
+ * GrblHoming - zapmaker fork on github
+ *
+ * 15 Nov 2012
+ * GPL License (see LICENSE file)
+ * Software is provided AS-IS
+ ****************************************************************/
+
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#include <QtGlobal>
+#include <QString>
+#include <QDateTime>
+#include "atomicintbool.h"
+
+#define DEFAULT_WAIT_TIME_SEC   100
+
+#define DEFAULT_Z_JOG_RATE      260.0
+#define DEFAULT_Z_LIMIT_RATE    100.0
+#define DEFAULT_XY_RATE         2000.0
+
+#define DEFAULT_GRBL_LINE_BUFFER_LEN    50
+
+#define MM_IN_AN_INCH           25.4
+#define PRE_HOME_Z_ADJ_MM       5.0
+
+#define REQUEST_CURRENT_POS             "?"
+#define SETTINGS_COMMAND_V08a           "$"
+#define SETTINGS_COMMAND_V08c           "$$"
+#define REQUEST_PARSER_STATE_V08c       "$G"
+#define SET_UNLOCK_STATE_V08c           "$X"
+
+#define REGEXP_SETTINGS_LINE    "(\\d+)\\s*=\\s*([\\w\\.]+)\\s*\\(([^\\)]*)\\)"
+
+#define OPEN_BUTTON_TEXT                "Open"
+#define CLOSE_BUTTON_TEXT               "Close / Reset"
+
+#define LOG_MSG_TYPE_DIAG       "DIAG"
+#define LOG_MSG_TYPE_STATUS     "STATUS"
+
+extern AtomicIntBool g_enableDebugLog;
+
+void status(const char *str, ...);
+void diag(const char *str, ...);
+void err(const char *str, ...);
+void warn(const char *str, ...);
+void info(const char *str, ...);
+
+#endif // DEFINITIONS_H
---
+++ src/mainwindow.h
@@ -0,0 +1,227 @@
+/****************************************************************
+ * mainwindow.h
+ * GrblHoming - zapmaker fork on github
+ *
+ * 15 Nov 2012
+ * GPL License (see LICENSE file)
+ * Software is provided AS-IS
+ ****************************************************************/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QFile>
+#include <QSettings>
+#include <QCloseEvent>
+#include <QItemDelegate>
+#include <QListView>
+#include "about.h"
+#include "definitions.h"
+#include "grbldialog.h"
+#include "options.h"
+//#include "filesender.h"
+#include "timer.h"
+#include "positem.h"
+#include "gcode.h"
+#include "renderarea.h"
+
+#define COMPANY_NAME "zapmaker"
+#define APPLICATION_NAME "GrblController"
+#define DOMAIN_NAME "org.zapmaker"
+
+#define TAB_AXIS_INDEX          0
+#define TAB_VISUALIZER_INDEX    1
+
+#define CENTER_POS              40
+
+/* testing optimizing scrollbar, doesn't work right
+class MyItemDelegate : public QItemDelegate
+{
+private:
+    int width;
+    QAbstractItemView *parentWidget;
+
+public:
+
+    MyItemDelegate(QAbstractItemView *p) : parentWidget(p) {}
+
+    void setWidth(int w)
+    {
+        width = w;
+    }
+
+    void drawDisplay(QPainter *painter,const
+    QStyleOptionViewItem &option,const QRect &rect,const QString &text) const{
+
+        QRect tempRect(rect);
+        tempRect.setWidth(parentWidget->width());
+        QItemDelegate::drawDisplay(painter,option,tempRect,text);
+
+    }
+
+    QSize sizeHint(const QStyleOptionViewItem & option, const
+    QModelIndex & index ) const  {
+
+        QListView *list = qobject_cast<QListView*>(parentWidget);
+        QSize newSize(QItemDelegate::sizeHint(option,index));
+        if( list ) newSize.setWidth( width );
+        return newSize;
+    }
+};
+*/
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void closeEvent(QCloseEvent *event);
+
+    //variables
+    int delete_nr;
+
+signals:
+    //threads
+    void openPort(QString port, QString baudRate);
+    void closePort(bool reopen);
+    void shutdown();
+    void sendGcode(QString line, bool recordResponseOnFail = false, int waitCount = SHORT_WAIT_SEC);
+    void sendFile(QString path);
+    void gotoXYZC(QString line);
+    void axisAdj(char axis, float coord, bool inv, bool absoluteAfterAxisAdj, int sliderZCount);
+    void setResponseWait(ControlParams controlParams);
+    void setProgress(int percent);
+    void setRuntime(QString runtime);
+    void sendSetHome();
+    void sendGrblReset();
+    void sendGrblUnlock();
+    void goToHome();
+    void setItems(QList<PosItem>);
+
+private slots:
+    //buttons
+    void openPort();
+    void setGRBL();
+        //Adjust
+    void decX();
+    void decY();
+    void decZ();
+	void decC();
+	void incC();
+    void incX();
+    void incY();
+    void incZ();
+    void setHome();
+        //manual
+    void gotoXYZC();
+        //send Gcode
+    void begin();
+    void openFile();
+    void stop();
+    void stopSending();
+    //
+    void portIsOpen(bool sendCode);
+    void portIsClosed(bool reopen);
+    void adjustedAxis();
+
+    //check boxes
+    void toggleSpindle();
+    void toggleRestoreAbsolute();
+
+    //communications
+        //options
+    void setSettings();
+        //thread
+    void receiveList(QString msg);
+    void receiveListFull(QStringList list);
+    void receiveListOut(QString msg);
+    void receiveMsg(QString msg);
+    //menu bar
+    void getOptions();
+    void showAbout();
+    void enableGrblDialogButton();
+    void grblReset();
+    void grblUnlock();
+    void updateCoordinates(Coord3D machineCoord, Coord3D workCoord);
+    void goHomeSafe();
+    void zJogSliderDisplay(int pos);
+    void zJogSliderPressed();
+    void zJogSliderReleased();
+
+private:
+    // enums
+    enum
+    {
+        NO_ITEM = 0,
+        X_ITEM,
+        Y_ITEM,
+        I_ITEM,
+        J_ITEM,
+    };
+    //objects
+    Ui::MainWindow *ui;
+    //FileSender fileSender;
+    //QThread fileSenderThread;
+    GCode gcode;
+    QThread gcodeThread;
+    Timer timer;
+    QThread timerThread;
+
+    //variables
+    bool invX;
+    bool invY;
+    bool invZ;
+	bool invC;
+	/// for translation
+	QString open_button_text ;
+	QString close_button_text ;
+    bool mm;
+    QString styleSheet;
+    QString directory;
+    QString nameFilter;
+    QString lastOpenPort;
+    QString lastBaudRate;
+    QByteArray fileOpenDialogState;
+    Coord3D machineCoordinates;
+    Coord3D workCoordinates;
+    bool absoluteAfterAxisAdj;
+    bool checkLogWrite;
+    QTime scrollStatusTimer;
+    QList<PosItem> posList;
+    bool sliderPressed;
+    double sliderTo;
+    int sliderZCount;
+    bool promptedAggrPreload;
+    ControlParams controlParams;
+
+    //methods
+    int SendJog(QString strline);
+    void readSettings();
+    void writeSettings();
+    void addToStatusList(bool in, QString msg);
+    void addToStatusList(QStringList& list);
+    void disableAllButtons();
+    void openPortCtl(bool reopen);
+    void resetProgress();
+    void refreshLcd();
+    void lcdDisplay(char axis, bool workCoord, float value);
+    void updateSettingsFromOptionDlg(QSettings& settings);
+    void doScroll();
+    int computeListViewMinimumWidth(QAbstractItemView* view);
+    void preProcessFile(QString filepath);
+    bool processGCode(QString inputLine, double& x, double& y, double& i, double& j, bool& arc, bool& cw, bool& mm, int& g);
+    double decodeLineItem(const QString& item, const int next, bool& valid, int& nextIsValue);
+    double decodeDouble(QString value, bool& valid);
+};
+
+
+#endif // MAINWINDOW_H
